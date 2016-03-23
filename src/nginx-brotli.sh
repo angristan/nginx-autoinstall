0059115843b9cb22e9338609f6f9def1b3225583
Angristan <stanislas.lange@openmailbox.org>
2016-03-23 17:29:28 +0100

    Update nginx-brotli.sh

--- src/nginx-brotli.sh
+++ src/nginx-brotli.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 NGINX_VER=1.9.12
-# Dependencies
 apt-get install build-essential ca-certificates libpcre3 libpcre3-dev autoconf automake libtool tar git libssl-dev -y
-# Brotli
 cd /opt
-# Cleaning up in case of update
 rm -r libbrotli
-# libbrolti is needed for the ngx_brotli module
-# libbrotli download
 git clone https://github.com/bagder/libbrotli
 cd libbrotli
 ./autogen.sh
 ./configure
 make -j $(nproc)
-# libbrotli install
 make install
-# Linking libraries to avoid errors
 ldconfig
-# ngx_brotli module download
 cd /opt
 git clone https://github.com/google/ngx_brotli
-# Nginx
 rm -r /opt/nginx-${NGINX_VER}
 cd /opt
 wget -qO- http://nginx.org/download/nginx-${NGINX_VER}.tar.gz | tar zxf -
