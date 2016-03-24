7e31f935ccb16f7897a7a84806a9d2e2cb04b1a1
Angristan <stanislas.lange@openmailbox.org>
2016-03-24 21:20:55 +0100

    Add http_fastcgi

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -107,7 +107,7 @@ cd /opt/nginx-${NGINX_VER}
 # Modules configuration
 # Common configuration 
 NGINX_OPTIONS="--prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --user=nginx --group=nginx"
-NGINX_MODULES="--without-http_ssi_module --without-http_scgi_module --without-http_uwsgi_module --without-http_fastcgi_module --without-http_geo_module --without-http_map_module --without-http_split_clients_module --without-http_memcached_module --without-http_empty_gif_module --without-http_browser_module --with-threads --with-file-aio --with-http_ssl_module --with-http_v2_module --with-ipv6 --with-http_mp4_module --with-http_auth_request_module --with-http_slice_module"
+NGINX_MODULES="--without-http_ssi_module --without-http_scgi_module --without-http_uwsgi_module --without-http_geo_module --without-http_map_module --without-http_split_clients_module --without-http_memcached_module --without-http_empty_gif_module --without-http_browser_module --with-threads --with-file-aio --with-http_ssl_module --with-http_v2_module --with-ipv6 --with-http_mp4_module --with-http_auth_request_module --with-http_slice_module"
 
 # No module
 if [[ "$LIBRESSL" = 'n' ]] && [[ "$PAGESPEED" = 'n' ]] && [[ "$BROTLI" = 'n' ]]; then
