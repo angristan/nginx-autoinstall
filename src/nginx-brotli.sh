aa3250da02e777e87d8c11815c2647388ce9f400
Angristan <stanislas.lange@openmailbox.org>
2016-04-10 12:03:33 +0200

    Update nginx-brotli.sh

--- src/nginx-brotli.sh
+++ src/nginx-brotli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-NGINX_VER=$(curl -s https://raw.githubusercontent.com/Angristan/nginx-autoinstall/master/var/nginx)
 apt-get install build-essential ca-certificates curl libpcre3 libpcre3-dev autoconf automake libtool tar git libssl-dev -y
+NGINX_VER=$(curl -s https://raw.githubusercontent.com/Angristan/nginx-autoinstall/master/var/nginx)
 cd /opt
 rm -r libbrotli
 git clone https://github.com/bagder/libbrotli
