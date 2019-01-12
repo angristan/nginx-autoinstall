bf44ee8377ee9f9cd587a9b20abcad5673324a4e
luison <joseluis@alsur.es>
2019-01-12 18:07:51 +0100

    Added support for a nginx-custom.options file that can optionally replace any of the 2 compiling vars for nginx with custom values and retain them over updates if file is ignored.

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -191,30 +191,53 @@ case $OPTION in
 		fi
 		cd /usr/local/src/nginx/nginx-${NGINX_VER} || exit 1
 
-		NGINX_OPTIONS="
-		--prefix=/etc/nginx \
-		--sbin-path=/usr/sbin/nginx \
-		--conf-path=/etc/nginx/nginx.conf \
-		--error-log-path=/var/log/nginx/error.log \
-		--http-log-path=/var/log/nginx/access.log \
-		--pid-path=/var/run/nginx.pid \
-		--lock-path=/var/run/nginx.lock \
-		--http-client-body-temp-path=/var/cache/nginx/client_temp \
-		--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
-		--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
-		--user=nginx \
-		--group=nginx \
-		--with-cc-opt=-Wno-deprecated-declarations"
+		# Modules default configuration
+		# Check if custom nginx configuration exists
+		if [ -f nginx-custom.options ]; then
+		    source nginx-custom.options
+		fi
+		 
+		if [ -z ${NGINX_OPTIONS+x} ];
+            then
+            echo "No NGINX_OPTIONS found, using default"
+            # Common configuration
+            NGINX_OPTIONS="
+            --prefix=/etc/nginx \
+            --sbin-path=/usr/sbin/nginx \
+            --conf-path=/etc/nginx/nginx.conf \
+            --error-log-path=/var/log/nginx/error.log \
+            --http-log-path=/var/log/nginx/access.log \
+            --pid-path=/var/run/nginx.pid \
+            --lock-path=/var/run/nginx.lock \
+            --http-client-body-temp-path=/var/cache/nginx/client_temp \
+            --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+            --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+            --user=nginx \
+            --group=nginx \
+            --with-cc-opt=-Wno-deprecated-declarations"
+	    fi
 
-		NGINX_MODULES="--with-threads \
-		--with-file-aio \
-		--with-http_ssl_module \
-		--with-http_v2_module \
-		--with-http_mp4_module \
-		--with-http_auth_request_module \
-		--with-http_slice_module \
-		--with-http_stub_status_module \
-		--with-http_realip_module"
+        if [ -z ${NGINX_MODULES+x} ];
+            then
+            echo "No NGINX_MODULES found, using default"
+            NGINX_MODULES="--without-http_ssi_module \
+			--without-http_scgi_module \
+			--without-http_uwsgi_module \
+			--without-http_geo_module \
+			--without-http_split_clients_module \
+			--without-http_memcached_module \
+			--without-http_empty_gif_module \
+			--without-http_browser_module \
+			--with-threads \
+			--with-file-aio \
+			--with-http_ssl_module \
+			--with-http_v2_module \
+			--with-http_mp4_module \
+			--with-http_auth_request_module \
+			--with-http_slice_module \
+			--with-http_stub_status_module \
+			--with-http_realip_module"
+		fi
 
 		# Optional modules
 		if [[ "$LIBRESSL" = 'y' ]]; then
