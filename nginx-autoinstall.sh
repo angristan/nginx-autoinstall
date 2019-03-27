e3fb89f514c498c290514e80eaf7289b64bcc311
Tommy Miland <kontakt@tmiland.com>
2019-03-27 01:16:58 +0100

    --with-http_dav_module
    Added --with-http_dav_module

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -307,7 +307,7 @@ case $OPTION in
 		
 		if [[ "$WEBDAV" = 'y' ]]; then
 			git clone --quiet https://github.com/arut/nginx-dav-ext-module.git /usr/local/src/nginx/modules/nginx-dav-ext-module
-			NGINX_MODULES=$(echo "$NGINX_MODULES"; echo --add-module=/usr/local/src/nginx/modules/nginx-dav-ext-module)
+			NGINX_MODULES=$(echo "$NGINX_MODULES"; echo --with-http_dav_module --add-module=/usr/local/src/nginx/modules/nginx-dav-ext-module)
 		fi
 
 		./configure $NGINX_OPTIONS $NGINX_MODULES
