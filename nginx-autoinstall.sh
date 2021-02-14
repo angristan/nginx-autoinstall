92c6610a9226e339188479705b0e8bebf072a674
Tommy Miland <kontakt@tmiland.com>
2021-02-14 10:55:58 +0100

    Reload systemctl (#171)
    Reload systemctl after uninstall.
    
    Uninstall without results in warning:
    
    `Warning: The unit file, source configuration file or drop-ins of nginx.service changed on disk. Run 'systemctl daemon-reload' to reload units.`

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -574,6 +574,9 @@ case $OPTION in
 		/lib/systemd/system/nginx.service \
 		/etc/systemd/system/multi-user.target.wants/nginx.service
 
+		# Reload systemctl
+		systemctl daemon-reload
+
 	# Remove conf files
 	if [[ $RM_CONF == 'y' ]]; then
 		rm -r /etc/nginx/
